6eb553943e6235c30dde7b32bb68c095c5d865ae
Hamid Shah <102824684+mbrown1837@users.noreply.github.com>
2024-09-18 05:01:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(
     page_icon=":blue_heart:",
 )
 st.title("Youtube Video Summaries powered by Groq")
-st.markdown("##### :blue_heart: built using [Hamid Shah:crown]")
+st.markdown("##### :blue_heart: built using [Hamid Shah👑]")
 
 # Groq API key is still retrieved here for the Groq model (but not for YouTubeTools)
 groq_api_key = st.secrets["groq_api_key"]
